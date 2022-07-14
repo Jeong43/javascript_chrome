927936da1f73d24ae17422b0257c36f0ac421126
mi-hwa <43jmih@naver.com>
2022-07-14 20:21:53 +0900

    22.07.14

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-const loginForm = document.getElementById("login-form");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+
+const link = document.querySelector("a");
+
+function onLoginBtnSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const userName = loginInput.value;
+
+    greeting.innerText = `Hello, ${userName}!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+function handleLinkClick(event) {
+    event.preventDefault();
+
+}
+
+
+loginForm.addEventListener("submit", onLoginBtnSubmit);
+link.addEventListener("click", handleLinkClick);
+
